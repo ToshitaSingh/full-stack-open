b8f1b706baf5416cb25d22ca7e997c7728a49b20
Toshita Singh <40301340+ToshitaSingh@users.noreply.github.com>
2023-05-26 19:30:27 +0530

    update solution to exercise 1.2

--- part1--introduction-to-react/courseinfo/src/App.js
+++ part1--introduction-to-react/courseinfo/src/App.js
@@ -10,13 +10,7 @@ const Part = (props) => {
   return (
     <div>
       <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
+        {props.part} {props.exercises}
       </p>
     </div>
   );
@@ -25,9 +19,9 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part part1={props.part1} exercises1={props.exercises1} />
-      <Part part2={props.part2} exercises2={props.exercises2} />
-      <Part part3={props.part3} exercises3={props.exercises3} />
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
     </div>
   );
 };
